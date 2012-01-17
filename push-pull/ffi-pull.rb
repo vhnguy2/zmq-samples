02c1b75bd3bfca64b958a3095f840c7784152254
Viet Nguyen <viet@ooyala.com>
2012-01-16 18:29:24 -0800

    [push-pull] Add ffi-rzmq examples of push-pull

--- push-pull/ffi-pull.rb
+++ push-pull/ffi-pull.rb
@@ -1,9 +1,10 @@
-require "zmq"
+require "ffi-rzmq"
 
 ctx = ZMQ::Context.new
 puller = ctx.socket(ZMQ::PULL)
 puller.connect("tcp://127.0.0.1:8001")
 
 loop do
-  puts puller.recv()
+  puller.recv_string(msg='')
+  puts msg
 end
--- push-pull/ffi-push.rb
+++ push-pull/ffi-push.rb
@@ -1,10 +1,10 @@
-require "zmq"
+require "ffi-rzmq"
 
 ctx = ZMQ::Context.new
 pusher = ctx.socket(ZMQ::PUSH)
 pusher.bind("tcp://127.0.0.1:8001")
 
 loop do
-  puts pusher.send("Hi.")
+  pusher.send_string("Hi.")
   sleep 1
 end
